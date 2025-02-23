1586e5d29ddb12f7792904d686acf27a78c0c4be
wen <alwjlola@gmail.com>
2025-02-23 20:05:00 +0800

    add logger

--- lib/src/util/log.dart
+++ lib/src/util/log.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart';
+import "package:logger/logger.dart" as loggerlib;
 import 'package:uizakura/uizakura.dart';
 
 /// @author luwenjie on 2024/2/27 15:43:53
@@ -6,14 +6,24 @@ import 'package:uizakura/uizakura.dart';
 class Logger {
   static void initialize() async {}
 
-  static void clear() {
-    // DataBase.logClear();
-  }
+  static void clear() {}
 
   static Future<void> print(String tag, String s) async {
     if (UizakuraAppEnv.isDebug) {
-      debugPrint(
+      debugLogger.d(
           "\n$tag:-------------------------\n$s\n-------------------------\n");
     }
+  }
+}
+
+final debugLogger = loggerlib.Logger(
+  filter: _DevelopmentFilter(),
+  printer: loggerlib.PrettyPrinter(methodCount: 1),
+);
+
+class _DevelopmentFilter extends loggerlib.LogFilter {
+  @override
+  bool shouldLog(loggerlib.LogEvent event) {
+    return UizakuraAppEnv.isDebug;
   }
 }
